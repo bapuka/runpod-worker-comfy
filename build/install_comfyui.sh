f8f8742fa6e0f1bafc5e4670ba8c3bdbf031f6d6
bapuka <bapuka@gmail.com>
2025-04-09 12:54:51 +0800

    changes

--- build/install_comfyui.sh
+++ build/install_comfyui.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Clone the repo
-cd /workspace
+cd /
 git clone --depth=1 https://github.com/comfyanonymous/ComfyUI.git 
 cd ComfyUI
 git checkout ${COMFYUI_VERSION}
@@ -27,38 +27,38 @@ git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-M
 cd custom_nodes/ComfyUI-Manager
 pip3 install -r requirements.txt
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone https://github.com/ntdviet/comfyui-ext.git custom_nodes/comfyui-ext
 cp custom_nodes/comfyui-ext/custom_nodes/gcLatentTunnel/gcLatentTunnel.py .
 rm -rf custom_nodes/comfyui-ext
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/rgthree/rgthree-comfy.git custom_nodes/rgthree-comfy
 cd custom_nodes/rgthree-comfy 
 pip3 install -r requirements.txt
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/griptape-ai/ComfyUI-Griptape.git custom_nodes/ComfyUI-Griptape
 cd custom_nodes/ComfyUI-Griptape 
 pip3 install -r requirements.txt
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/ltdrdata/ComfyUI-Impact-Pack.git custom_nodes/ComfyUI-Impact-Pack
 cd custom_nodes/ComfyUI-Impact-Pack 
 pip3 install -r requirements.txt
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/WASasquatch/was-node-suite-comfyui custom_nodes/was-node-suite-comfyui
 cd custom_nodes/was-node-suite-comfyui 
 pip3 install -r requirements.txt
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/cubiq/ComfyUI_IPAdapter_plus.git custom_nodes/ComfyUI_IPAdapter_plus
 git clone --depth 1 https://github.com/cubiq/ComfyUI_InstantID.git custom_nodes/ComfyUI_InstantID
 cd custom_nodes/ComfyUI_InstantID 
 pip3 install -r requirements.txt
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/cubiq/PuLID_ComfyUI.git custom_nodes/PuLID_ComfyUI
 cd custom_nodes/PuLID_ComfyUI 
 pip3 install -r requirements.txt
@@ -66,37 +66,37 @@ pip3 install -r requirements.txt
 # git clone --depth 1 https://github.com/Gourieff/comfyui-reactor-node.git custom_nodes/comfyui-reactor-node
 # cd custom_nodes/comfyui-reactor-node 
 # pip3 install -r requirements.txt
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/Extraltodeus/ComfyUI-AutomaticCFG.git custom_nodes/ComfyUI-AutomaticCFG
 cd custom_nodes/ComfyUI-AutomaticCFG 
 pip3 install -r requirements.txt
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/Extraltodeus/pre_cfg_comfy_nodes_for_ComfyUI.git custom_nodes/pre_cfg_comfy_nodes_for_ComfyUI
 git clone --depth 1 https://github.com/crystian/ComfyUI-Crystools.git custom_nodes/ComfyUI-Crystools
 cd custom_nodes/ComfyUI-Crystools 
 pip3 install -r requirements.txt
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone --depth 1 https://github.com/XLabs-AI/x-flux-comfyui.git custom_nodes/x-flux-comfyui
 cd custom_nodes/x-flux-comfyui 
 python3 setup.py
 
-cd /workspace/ComfyUI
+cd /ComfyUI
 git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git custom_nodes/comfyui_controlnet_aux
 cd custom_nodes/comfyui_controlnet_aux 
 pip3 install -r requirements.txt
 
 echo "Downloading SDXL Refiner"
-cd /workspace/ComfyUI/models/checkpoints
+cd /ComfyUI/models/checkpoints
 wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
 
 echo "Downloading SDXL VAE"
-cd /workspace/ComfyUI/models/vae
+cd /ComfyUI/models/vae
 wget https://huggingface.co/madebyollin/sdxl-vae-fp16-fix/resolve/main/sdxl_vae.safetensors
 
 echo "Creating log directory"
-mkdir -p /workspace/logs
+mkdir -p /logs
 
 pip3 cache purge
 deactivate
--- src/start.sh
+++ src/start.sh
@@ -9,18 +9,18 @@ export HF_HOME="/"
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "runpod-worker-comfy: Starting local ComfyUI"
-    cd /workspace/ComfyUI
+    cd /ComfyUI
     source venv/bin/activate
-    python3 main.py --disable-auto-launch --disable-metadata --listen --port 3001 > /workspace/logs/comfyui.log 2>&1 &
+    python3 main.py --disable-auto-launch --disable-metadata --listen --port 3001 > /logs/comfyui.log 2>&1 &
     deactivate
     
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
     echo "runpod-worker-comfy: Starting ComfyUI"    
-    cd /workspace/ComfyUI
+    cd /ComfyUI
     source venv/bin/activate
-    python3 main.py --disable-auto-launch --disable-metadata --listen --port 3001 > /workspace/logs/comfyui.log 2>&1 &
+    python3 main.py --disable-auto-launch --disable-metadata --listen --port 3001 > /logs/comfyui.log 2>&1 &
     deactivate
     
     echo "runpod-worker-comfy: Starting RunPod Handler"
