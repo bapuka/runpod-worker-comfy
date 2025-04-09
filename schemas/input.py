646c063ac032a0192c4012240a4d1e8b11c6fd43
bapuka <bapuka@gmail.com>
2025-04-09 17:41:39 +0800

    changes

---
+++ schemas/input.py
@@ -0,0 +1,17 @@
+INPUT_SCHEMA = {
+    'workflow': {
+        'type': str,
+        'required': False,
+        'default': 'txt2img',
+        'constraints': lambda workflow: workflow in [
+            'default',
+            'txt2img',
+            'img2imgPersona',
+            'custom'
+        ]
+    },
+    'payload': {
+        'type': dict,
+        'required': True
+    }
+}
