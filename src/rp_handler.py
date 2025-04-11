218d51aad444303fd9e54c5b3a7a1646edf9c0f9
bapuka <bapuka@gmail.com>
2025-04-11 19:45:51 +0800

    changes

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -150,7 +150,7 @@ def check_server(url, retries=500, delay=50):
     return False
 
 def get_workflow_payload(workflow_name, payload, image_names=None):
-    with open(f'src/workflows/{workflow_name}.json', 'r') as json_file:
+    with open(f'workflows/{workflow_name}.json', 'r') as json_file:
         workflow = json.load(json_file)
 
     if workflow_name == 'img2imgPersona':
