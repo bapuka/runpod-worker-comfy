104ca034970b4384a9136668fcc94b0384dab74a
bapuka <bapuka@gmail.com>
2025-05-16 12:28:16 +0800

    changes

--- src/upscaler.py
+++ src/upscaler.py
@@ -118,7 +118,7 @@ from nodes import NODE_CLASS_MAPPINGS
 def queue(image="", scale=2):
     import subprocess
     result = subprocess.run(['source', '/ComfyUI/venv/bin/activate'], shell=True, check=True)
-    
+    print(f"Activated virtual environment: '{result}'")
     import_custom_nodes()
     with torch.inference_mode():
         upscalemodelloader = NODE_CLASS_MAPPINGS["UpscaleModelLoader"]()
