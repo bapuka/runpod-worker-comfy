e89d3ce555c91f208371d25bd7e1cd1bab10d56d
bapuka <bapuka@gmail.com>
2025-05-18 13:06:59 +0800

    changes

--- build/install_comfyui.sh
+++ build/install_comfyui.sh
@@ -86,9 +86,11 @@ cd /ComfyUI
 git clone https://github.com/shiimizu/ComfyUI_smZNodes.git custom_nodes/ComfyUI_smZNodes
 git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git custom_nodes/ComfyUI-Custom-Scripts
 git clone https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git custom_nodes/ComfyUI_UltimateSDUpscale --recursive
-git clone https://github.com/yolain/ComfyUI-Easy-Use.git custom_nodes/ComfyUI-Easy-Use
-/usr/bin/sh ./custom_nodes/ComfyUI-Easy-Use/install.sh
 git clone https://github.com/shiimizu/ComfyUI-TiledDiffusion.git custom_nodes/ComfyUI-TiledDiffusion
+git clone https://github.com/yolain/ComfyUI-Easy-Use.git custom_nodes/ComfyUI-Easy-Use
+cd custom_nodes/ComfyUI-Easy-Use
+/usr/bin/sh install.sh
+
 
 cd /ComfyUI
 git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git custom_nodes/comfyui_controlnet_aux
